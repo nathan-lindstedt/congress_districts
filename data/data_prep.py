df05c9cb3ec088c77ecc74dcb21d58849b243f2f
nathan-lindstedt <iconseq@gmail.com>
2024-11-17 13:49:20 -0700

    Refactored TuncatedSVD() and top feature contributions. Improved PEP 8 compliance and code readability.

--- data/data_prep.py
+++ data/data_prep.py
@@ -308,13 +308,92 @@ def create_indicator_columns(df: pd.DataFrame, cols: List) -> pd.DataFrame:
 
     return pd.concat([df, pd.DataFrame(df[cols].isnull().astype(int).add_suffix('_indicator'))], axis=1).fillna(0)
 
+def reduce_dimensions_svd(X_train: pd.DataFrame, X_test: pd.DataFrame, common_cols: List[str]) -> tuple:
+    """
+    Reduce the dimensionality of the training and testing datasets using Truncated SVD.
+
+    Args:
+        X_train (pd.DataFrame): The training dataset.
+        X_test (pd.DataFrame): The testing dataset.
+        common_cols (list): List of common columns to be used for SVD.
+
+    Returns:
+        model_svd (TruncatedSVD): The trained TruncatedSVD model.
+        X_train_svd (pd.DataFrame): The transformed training dataset.
+        X_test_svd (pd.DataFrame): The transformed testing dataset.
+
+    Notes:
+        SVD suffers from "sign indeterminacy", meaning the sign of the components_
+        and the output from transform depend on the algorithm and random state.
+        To work around this, fit and transform instances of the training data with
+        fit_transform(), then keep the trained instance around to do subsequent
+        transformations of the testing data with transform(). Thus, the absolute value
+        of the contribution is used in generate_feature_importance() to determine the 
+        top features by magnitude, whether positive or negative.
+
+        (See: https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.TruncatedSVD.html)
+    """
+
+    svd_performance = []
+    base = X_train.shape[0]//10
+    scree = range(1, X_train.shape[1]//base)
+
+    for n in scree:
+        scree_svd = TruncatedSVD(n_components=n, random_state=42) 
+        scree_fit = scree_svd.fit(X_train[common_cols])
+        svd_performance.append(scree_fit.explained_variance_ratio_.sum())
+
+    n_components = len([x for x in svd_performance if x <= .975])
+    model_svd = TruncatedSVD(n_components=n_components)
+    
+    X_train_svd = pd.DataFrame(model_svd.fit_transform(X_train[common_cols]))
+    X_test_svd = pd.DataFrame(model_svd.transform(X_test[common_cols]))
+    
+    return model_svd, X_train_svd, X_test_svd
+
+def generate_feature_importance(common_cols: List[str], model_svd: TruncatedSVD) -> pd.DataFrame:
+    """
+    Generate a DataFrame containing the top feature contributions for each component 
+    from the Truncated SVD model.
+
+    Args:
+        common_cols (list): List of common columns used in the SVD.
+        model_svd (TruncatedSVD): The trained TruncatedSVD model.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the top feature contributions for each component.
+                      The DataFrame has columns 'Component', 'Feature', and 'Contribution'.
+
+    Summary:
+        The function sorts the features by their absolute contribution values and selects 
+        the top N features for each component.
+    """
+
+    feature_contributions_list: List = []
+    top_n: int = 10
+
+    components = model_svd.components_
+
+    for i, component in enumerate(components):
+        feature_contributions = sorted(zip(common_cols, component), key=lambda x: abs(x[1]), reverse=True)
+        for feature, contribution in feature_contributions[:top_n]:
+            feature_contributions_list.append({
+            'Component': i + 1,
+            'Feature': feature,
+            'Contribution': contribution
+        })
+
+    return pd.DataFrame(feature_contributions_list)
+
 
 #%%
 # Load the data
 X_train = pd.read_csv(os.path.relpath(f'nhgis0002_ds249_20205_cd116th.csv', start=start), low_memory=False)
 X_test = pd.read_csv(os.path.relpath(f'nhgis0001_ds262_20225_cd118th.csv', start=start), low_memory=False)
+
 y = pd.read_csv(os.path.relpath(f'house_outcomes_historical.csv', start=start), low_memory=False)
 y['party'] = y['party'].str[:3]
+
 poll = pd.read_csv(os.path.relpath(f'538_house_polls_historical.csv', start=start), low_memory=False).rename(columns={'seat_number':'district'})
 poll['state_po'] = state_to_postal(poll)
 
@@ -327,61 +406,24 @@ dict_cd118 = get_dict(os.path.relpath(f'nhgis0001_ds262_20225_cd118th_codebook.t
 # Rename columns
 X_train_gisjoin = pd.DataFrame(X_train['GISJOIN'])
 X_test_gisjoin = pd.DataFrame(X_test['GISJOIN'])
+
 X_train_cols = [col for col in X_train.columns if col not in context_fields]
 X_test_cols = [col for col in X_test.columns if col not in context_fields]
+
 X_train_rename, X_train = rename_df_columns(X_train[X_train_cols], dict_cd116)
 X_test_rename, X_test = rename_df_columns(X_test[X_test_cols], dict_cd118)
 
 #%%
 # Keep common columns
 common_cols = list(set(X_train.columns) & set(X_test.columns))
+
 X_train = X_train[common_cols]
 X_test = X_test[common_cols]
 
 #%%
-# Truncated SVD w/ automatic component selection for dimensionality reduction
-svd_performance = []
-base = X_train.shape[0]//10
-scree = range(1, X_train.shape[1]//base)
-
-for n in scree:
-    scree_svd = TruncatedSVD(n_components=n, random_state=42) 
-    scree_fit = scree_svd.fit(X_train[common_cols])
-    svd_performance.append(scree_fit.explained_variance_ratio_.sum())
-
-n_components = len([x for x in svd_performance if x <= .975])
-model_svd = TruncatedSVD(n_components=n_components)
-X_train_svd = pd.DataFrame(model_svd.fit_transform(X_train[common_cols]))
-X_test_svd = pd.DataFrame(model_svd.transform(X_test[common_cols]))
-
-#%%
-# Extract top feature contributions of selected components from Truncated SVD
-
-# Note: SVD suffers from "sign indeterminacy", meaning the sign of the components_ 
-# and the output from transform depend on the algorithm and random state. 
-# To work around this, fit instances of this class to data once, then keep the 
-# instance around to do transformations. Thus, the absolute value of the contribution
-# is used to determine the top features for each component.
-
-# (See: https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.TruncatedSVD.html)
-
-feature_contributions_list: List = []
-top_n: int = 10
-
-components = model_svd.components_
-component_df = pd.DataFrame(components, columns=common_cols)
-
-for i, component in enumerate(components):
-    feature_contributions = sorted(zip(common_cols, component), key=lambda x: abs(x[1]), reverse=True)
-    for feature, contribution in feature_contributions[:top_n]:
-        feature_contributions_list.append({
-            'Component': i + 1,
-            'Feature': feature,
-            'Contribution': contribution
-        })
-
-feature_contributions_df = pd.DataFrame(feature_contributions_list)
-feature_contributions_df.to_csv(os.path.relpath(f'../output/top_feature_contributions.csv', start=start), index=False)
+# Truncated SVD w/ component selection for dimensionality reduction and feature contribution
+model_svd, X_train_svd, X_test_svd = reduce_dimensions_svd(X_train, X_test, common_cols)
+feature_contributions_df = generate_feature_importance(common_cols, model_svd)
 
 #%%
 # Preprocessing
@@ -389,13 +431,24 @@ X_train = pd.concat([X_train_gisjoin, X_train_svd], axis=1)
 X_test = pd.concat([X_test_gisjoin, X_test_svd], axis=1)
 
 y['GISJOIN'] = 'G' + y['state_fips'].astype(str).str.zfill(2) + y['district'].astype(str).str.zfill(3)
-y_train = y[(y['year'] == 2020) & (y['candidatevotes'] == y.groupby(['GISJOIN', 'year'])['candidatevotes'].transform('max'))]
-y_test = y[(y['year'] == 2022) & (y['candidatevotes'] == y.groupby(['GISJOIN', 'year'])['candidatevotes'].transform('max'))]
 
-avg_train = preprocess_poll_data(poll, 2020).merge(y_train[['state_po', 'district', 'GISJOIN']], 
-                                                   on=['state_po', 'district']).pivot(index='GISJOIN', columns='party', values='pct').reset_index()
-avg_test = preprocess_poll_data(poll, 2022).merge(y_test[['state_po', 'district', 'GISJOIN']], 
-                                                  on=['state_po', 'district']).pivot(index='GISJOIN', columns='party', values='pct').reset_index()
+y_train = y[
+    (y['year'] == 2020) & 
+    (y['candidatevotes'] == y.groupby(['GISJOIN', 'year'])['candidatevotes'].transform('max'))
+]
+
+y_test = y[
+    (y['year'] == 2022) & 
+    (y['candidatevotes'] == y.groupby(['GISJOIN', 'year'])['candidatevotes'].transform('max'))
+]
+
+avg_train = preprocess_poll_data(poll, 2020).merge(
+    y_train[['state_po', 'district', 'GISJOIN']], on=['state_po', 'district']
+).pivot(index='GISJOIN', columns='party', values='pct').reset_index()
+
+avg_test = preprocess_poll_data(poll, 2022).merge(
+    y_test[['state_po', 'district', 'GISJOIN']], on=['state_po', 'district']
+).pivot(index='GISJOIN', columns='party', values='pct').reset_index()
 
 y_train = pd.get_dummies(y_train, prefix=['party'], columns=['party']).query('totalvotes > 0')
 y_test = pd.get_dummies(y_test, prefix=['party'], columns=['party']).query('totalvotes > 0')
@@ -408,6 +461,7 @@ test_set = create_indicator_columns(y_test.merge(X_test, on='GISJOIN'), party_co
 
 #%%
 # Export data
+feature_contributions_df.to_csv(os.path.relpath(f'../output/top_feature_contributions.csv', start=start), index=False)
 train_set.to_csv(os.path.relpath(f'../output/train_set.csv', start=start), index=False)
 test_set.to_csv(os.path.relpath(f'../output/test_set.csv', start=start), index=False)
 
